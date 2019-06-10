9a5c2968659f6f8864110a5d3062bcb8510f0289
Alex <alexphp47@gmail.com>
2019-06-10 16:54:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/intentstutorial/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.intentstutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Toast.makeText(getApplicationContext(),
+                "We are moved to second Activity",Toast.LENGTH_LONG).show();
+    }
+}
